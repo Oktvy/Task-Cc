a9af96fb6d9efb61c1d7b3d94bc99cfc27962e13
oktvy <oktaynm@code.edu.az>
2022-11-13 11:08:00 -0800

    C#

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿#region 2-nin Quvveti olub olmadigini tapan alqoritm
+//Console.WriteLine("Please choose number");
+//String numStr = Console.ReadLine();
+//int num = Convert.ToInt32(numStr);
+//while (num % 2 == 0)
+//{
+//    num = num / 2;
+//}
+//if (num == 1)
+//{
+//    Console.WriteLine("2-nin Quvvetidir");
+//}
+//else
+//{
+//    Console.WriteLine("2-nin Quvveti deyil");
+//}
+
+#endregion
+
+
+#region Iki eded arasindaki ededleri tapan alqoritm
+//Console.WriteLine("Please choose number");
+//int n = Convert.ToInt32(Console.ReadLine());
+//int m = Convert.ToInt32(Console.ReadLine());
+
+//for (int i = n; i < m; i++)
+//{
+//   Console.WriteLine(i);
+//} 
+#endregion
+
+
+#region X Y Z en boyuyunu tapan alqoritm
+// Console.WriteLine("Please choose number");
+// int x = Convert.ToInt32(Console.ReadLine());
+// int y = Convert.ToInt32(Console.ReadLine());
+// int z = Convert.ToInt32(Console.ReadLine());
+
+//if (x > y && x > z)
+//{
+//    Console.WriteLine(x);
+//}
+// if  (y > x && y > z)
+//{
+//    Console.WriteLine(y);
+
+//}
+//else
+//{
+//    Console.WriteLine(z);
+//} 
+#endregion
+
+
+#region 70den boyuk kicik
+//Console.WriteLine("Please choose number");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int i = 70;
+//if (num > i)
+//{
+//    num = num - i;
+//    num = num * 3;
+//    Console.WriteLine(num);
+//}
+//else
+//{
+//    i = num - i;
+//    Console.WriteLine(i) ;
+//} 
+#endregion
+
+
+#region iki eded arasinda tek ededlerin sayini gosteren alqoritm
+// Console.WriteLine("Please choose number");
+//int n = Convert.ToInt32(Console.ReadLine());
+//int m = Convert.ToInt32(Console.ReadLine());
+//int count = 0;
+//for (int i = n; i < m; i++) 
+//{
+//    if (i % 2 == 1)
+//    {
+//        count++;
+
+//    }
+//}
+//Console.WriteLine(count); 
+#endregion
+
+
+
+
+
+
+
+
+
